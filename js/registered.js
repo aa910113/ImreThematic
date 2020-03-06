24972f4cc814104c3529d48e7b2339ccb026032d
Kang-zh <paris267@gmail.com>
2020-03-07 04:04:10 +0800

    新增註冊功能也可寄送驗證信
    目前可以寄送驗證信
    也可註冊  但 還未寫入後端給的錯誤題是
    寄送驗證信要給為只有number

--- js/registered.js
+++ js/registered.js
@@ -3,53 +3,66 @@ var app = new Vue({
   data:{
     Email: "",
     Password: "",
-    Verification: "" 
+    PasswordAgain: "",
+    Verification: "" ,
+    checked:false,
   },
   methods:{
     sendEmail(){
-      axios.post('http://imrebooksystem.southeastasia.cloudapp.azure.com/Home/Shit', { "Email": "bbb@gmail.com" })
+      const cors = "https://cors-anywhere.herokuapp.com/"; 
+      const url = "http://imrebooksystem.southeastasia.cloudapp.azure.com/businessUsers/VerificationMailSend";
+      if (this.Email === '') {
+        alert("請填寫E-mail");
+      } else if (this.Email !== '') {
+        var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        if (!reg.test(this.Email)) {
+          alert("請输入有效的E-mail");
+        } else {
+          // bbb@gmail.com
+          axios.post(`${cors}${url}`, { "Email": this.Email })
+            .then((res) => { 
+              localStorage.setItem("Verification code", res.data)
+              // console.table(res.data) 
+            })
+            .catch((error) => { console.error(error) })
+        }
+      }
+    },
+    registered(){
+      const cors = "https://cors-anywhere.herokuapp.com/";
+      const url = "http://imrebooksystem.southeastasia.cloudapp.azure.com/BusinessUsers/create";
+      if (this.Email === '') {
+        alert("請填寫E-mail");
+      } else if (this.Email !== '') {
+        var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        if (!reg.test(this.Email)) {
+          alert("請输入有效的E-mail");
+        } else {
+          if(this.Password===''){
+            alert("請輸入密碼")
+          }else if (this.PasswordAgain === '') {
+            alert("再輸入一次密碼")
+          }else if(this.Password!==this.PasswordAgain){
+            alert("密碼不相同")
+          }else{
+            if (this.checked===false){
+              alert("請勾選我同意")
+            }else{
+              // localStorage.getItem("name")
+              console.log(localStorage.getItem("Verification code"))
+              // console.log(this.Password.length)
+              // axios.post(`${cors}${url}`, {
+              //   "Email": "bbb@gmail.com",
+              //   "Password": "bbbb",
+              //   "Verification": "4099"
+              // })
+              //   .then((res) => { console.table(res.data) })
+              //   .catch((error) => { console.error(error) })
+            }
+          }
+        }
+      }
     }
-    // sendEmail:function () {
-
-    //   // if(this.Email === '') {
-    //   //   alert("請正確填寫E-mail");
-    //   // }else if (this.Email !== ''){
-    //   //   var reg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-    //   //   if (!reg.test(this.Email)) {
-    //   //     alert("請输入有效的E-mail");
-    //   //   }else{
-    //   //       // bbb@gmail.com
-    //   //     var raw = JSON.stringify({ "Email": "bbb@gmail.com" });
-    //   //     axios.post("http://imrebooksystem.southeastasia.cloudapp.azure.com/businessUsers/VerificationMailSend", raw)
-    //   //       .then(function (response) {
-    //   //         console.log(response);
-    //   //       })
-    //   //       .catch(function (error) {
-    //   //         console.log(error);
-    //   //       });
-    //       // var myHeaders = new Headers();
-    //       // myHeaders.append("Content-Type", "application/json");
-
-    //       // var raw = JSON.stringify({ "Email": this.Email });
-    //       // console.log(raw)
-
-    //       // var requestOptions = {
-    //       //   method: 'POST',
-    //       //   headers: myHeaders,
-    //       //   body: raw,
-    //       //   redirect: 'follow'
-    //       // };
-
-    //       // fetch("http://imrebooksystem.southeastasia.cloudapp.azure.com/businessUsers/VerificationMailSend", requestOptions)
-    //       //   .then(response => response.text())
-    //       //   .then(result => console.log(result))
-    //       //   .catch(error => console.log('error', error));
-    //     }
-    //   }
-        
-      
-    // }
-    
   }
 });
 
